7a41e3a25e2f81543e40fbe52d814f10dae8f127
skyparticle25 <skyparticle25@gmail.com>
2022-01-08 19:30:50 +0700

    Started settings script

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -8,5 +8,24 @@ using UnityEngine;
 
 public static class GameSettings 
 {
-    
+    // platforms 
+	public static float platformSize; 
+	public static float platformSpeed; 
+	// ai 
+	public static float aiAccuracy; 
+
+
+
+
+	//  Saving / loading  ------------------------------------------- 
+	public static void Save () 
+	{
+
+	}
+
+	public static void Load () 
+	{
+		
+	}
+
 }
